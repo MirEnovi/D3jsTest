f68c2609f670675a72058fc58447264090d16178
Mirian <mir@teoapp.co>
2020-08-24 14:59:10 -0500

    configutate d3

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import * as d3 from 'd3'
 
 const chart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const divRef = useRef(null);
+	useEffect(() => {
+		let accessToRef = d3.selectAll(divRef.current);
+	}, [])
+
+
+	let data = [12, 36, 6, 25, 35, 10, 20];
+
+	let width = 500; // <-- 1
+	let height = 500;
+
+	const accessToRef = d3.select(divRef.current)
+		.append('svg')
+		.attr("width", width) // <-- 2
+		.attr("height", height);
+	
+	accessToRef.selectAll('rect')
+		.data(data)
+		.enter()
+		.append('rect')
+		.attr('x', (d, i) => i * 70) 
+		.attr('y', (d, i) => h - 10 * 70); 
+		
+
+
+  	return <svg ref={divRef}></svg>;
+};
 
 export default chart
